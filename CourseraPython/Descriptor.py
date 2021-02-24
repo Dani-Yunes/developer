b308ca4c44403c4ace53481190997fa412d3bd4b
Dani-Yunes <60981549+Dani-Yunes@users.noreply.github.com>
2021-02-24 23:23:28 +0300

    Add files via upload

---
+++ CourseraPython/Descriptor.py
@@ -0,0 +1,13 @@
+class Value:
+    def __get__(self, obj, obj_type):
+        return self.value
+
+    def __set__(self, obj, value):
+        self.value = value * (1 - obj.commission)
+
+
+class Account:
+    amount = Value()
+
+    def __init__(self, commission):
+        self.commission = commission
